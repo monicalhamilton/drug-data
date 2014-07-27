d0bd125de5f36c17354636ab80aef6601ddf9191
monicalhamilton <monicalhamilton@gmail.com>
2014-07-27 11:56:04 -0400

    convert to maven project

--- src/main/java/file/DrugFileParser.java
+++ src/main/java/file/DrugFileParser.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 import api.AdministrationInstance;
 import api.SingleDrugAdministration;
 
